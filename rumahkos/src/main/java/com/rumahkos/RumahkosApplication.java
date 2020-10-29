858b82fb7b5150c6fda672a1e8b844e1a64b8237
fluex404 <isaabqari@gmail.com>
2020-10-29 16:54:49 +0700

    ngepush backend

---
+++ rumahkos/src/main/java/com/rumahkos/RumahkosApplication.java
@@ -0,0 +1,13 @@
+package com.rumahkos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahkosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahkosApplication.class, args);
+	}
+
+}
---
+++ rumahkos/src/test/java/com/rumahkos/RumahkosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rumahkos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahkosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
